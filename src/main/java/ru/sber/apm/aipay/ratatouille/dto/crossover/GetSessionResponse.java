572834e0ed59f145d7b5f5dd144958abd30466a4
glaswennn <golovinov.daniel@gmail.com>
2026-03-29 15:38:44 +0300

    prompts

--- src/main/java/ru/sber/apm/aipay/ratatouille/dto/crossover/GetSessionResponse.java
+++ src/main/java/ru/sber/apm/aipay/ratatouille/dto/crossover/GetSessionResponse.java
@@ -1,9 +1,12 @@
 package ru.sber.apm.aipay.ratatouille.dto.crossover;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.Accessors;
+import tools.jackson.databind.annotation.JsonSerialize;
 
 @Getter
 @Setter
@@ -18,5 +21,7 @@ public class GetSessionResponse {
     private String sessionId;
 
     @NotBlank
+    @JsonProperty("crossoverOrderId")      // Имя поля в JSON при сериализации
+    @JsonAlias("orderId")                 // Альтернативное имя при десериализации
     private String orderId;
 }
